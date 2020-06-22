333d0bc2d21575a8f103e217de1130e61d247084
re5nardo <re5nardo@naver.com>
2020-06-22 22:30:42 +0900

    [배인수] 1. 주변 Entity Transform 값 변경 시 약간의 interval 두었던 것 제거

--- Assets/Scripts/EntityComponent/Components/TransformAgent.cs
+++ Assets/Scripts/EntityComponent/Components/TransformAgent.cs
@@ -5,12 +5,12 @@ using GameFramework;
 
 public class TransformAgent : MonoComponentBase
 {
-    public int WaitingInterval = 3;  //  최소 대기 시간(tick) (이 시간 이하로는 다시 send 불가)
-
-    public Vector3 LastPosition;
-    public Vector3 LastRotation;
-    public Vector3 LastVelocity;
-    public Vector3 LastAngularVelocity;
+    public int WaitingInterval = 0;  //  최소 대기 시간(tick) (이 시간 이하로는 다시 send 불가)
+  
+    private Vector3 lastPosition;
+    private Vector3 lastRotation;
+    private Vector3 lastVelocity;
+    private Vector3 lastAngularVelocity;
 
     public int LastSendTick = -1;
 
@@ -18,19 +18,19 @@ public class TransformAgent : MonoComponentBase
     {
         get
         {
-            return  LastPosition != Entity.Position ||
-                    LastRotation != Entity.Rotation ||
-                    LastVelocity != Entity.Velocity ||
-                    LastAngularVelocity != Entity.AngularVelocity;
+            return lastPosition != Entity.Position ||
+                    lastRotation != Entity.Rotation ||
+                    lastVelocity != Entity.Velocity ||
+                    lastAngularVelocity != Entity.AngularVelocity;
         }
         set
         {
             if (value == false)
             {
-                LastPosition = Entity.Position;
-                LastRotation = Entity.Rotation;
-                LastVelocity = Entity.Velocity;
-                LastAngularVelocity = Entity.AngularVelocity;
+                lastPosition = Entity.Position;
+                lastRotation = Entity.Rotation;
+                lastVelocity = Entity.Velocity;
+                lastAngularVelocity = Entity.AngularVelocity;
             }
         }
     }
@@ -39,10 +39,10 @@ public class TransformAgent : MonoComponentBase
     {
         base.OnAttached(entity);
 
-        LastPosition = Entity.Position;
-        LastRotation = Entity.Rotation;
-        LastVelocity = Entity.Velocity;
-        LastAngularVelocity = Entity.AngularVelocity;
+        lastPosition = Entity.Position;
+        lastRotation = Entity.Rotation;
+        lastVelocity = Entity.Velocity;
+        lastAngularVelocity = Entity.AngularVelocity;
 
         LastSendTick = Game.Current.CurrentTick;
     }
