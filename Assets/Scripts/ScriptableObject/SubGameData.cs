0e3aa3f396e643745efeac0a48cce6253baca885
re5nardo <re5nardo@naver.com>
2021-01-07 00:36:50 +0900

    [배인수] 1. SubGameData 및 씬 추가

---
+++ Assets/Scripts/ScriptableObject/SubGameData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SubGameData", menuName = "ScriptableObjects/SubGameData", order = 1)]
+public class SubGameData : ScriptableObject
+{
+    public string id;
+    public Object scene;
+}
