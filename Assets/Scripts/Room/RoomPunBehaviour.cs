3afced3adb414534e18633762e6043c5f59eec70
re5nardo <re5nardo@naver.com>
2021-01-10 20:40:15 +0900

    [배인수] 1. 예정된 플레이어가 아닌 플레이어가 룸에 입장했을 때 kick 하도록 수정

--- Assets/Scripts/Room/RoomPunBehaviour.cs
+++ Assets/Scripts/Room/RoomPunBehaviour.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon;
+using System.Linq;
 
 public class RoomPunBehaviour : PunBehaviour
 {
     public override void OnPhotonPlayerConnected(PhotonPlayer newPlayer)
     {
+        if (PhotonNetwork.room.ExpectedUsers != null && !PhotonNetwork.room.ExpectedUsers.Contains(newPlayer.UserId))
+        {
+            PhotonNetwork.CloseConnection(newPlayer);
+
+            Debug.LogError("Invalid user enter! UserId : " + newPlayer.UserId);
+            return;
+        }
+
         RoomPubSubService.Publish(RoomMessageKey.PlayerEnter, newPlayer);
     }
 
