d6daad08c76d45bc665e640a682e86d50fb5fb79
re5nardo <re5nardo@naver.com>
2021-05-07 11:29:26 +0900

    [배인수] 1. 물리 기반으로 동작하도록 수정

--- Assets/Scripts/EntityComponent/Behavior/Behaviors/Move.cs
+++ Assets/Scripts/EntityComponent/Behavior/Behaviors/Move.cs
@@ -8,6 +8,7 @@ namespace Behavior
     public class Move : BehaviorBase
     {
         private Vector3 m_vec3Destination;
+        private int remainCount = 3;
 
         #region ISynchronizable
         protected override ISnap LastSendSnap { get; set; } = new MoveSnap();
@@ -26,41 +27,43 @@ namespace Behavior
 
         protected override bool OnBehaviorUpdate()
         {
-			Vector3 toMove = m_vec3Destination - Entity.Position;
+			Vector3 toMove = m_vec3Destination.XZ() - Entity.Position.XZ();
 
-            //  회전량에 따라 감속하는 정도를 다르게?
-            //var angle = Vector3.Angle(Entity.Forward, toMove);
-            //Entity.Velocity *=  Mathf.Lerp(0.8f, 0.5f, angle / 180);
-
-            Entity.Velocity *= 0.9f;
-            Entity.Velocity += (toMove.normalized * Entity.MovementSpeed * 0.2f);
-
-            if (Entity.Velocity.magnitude > Entity.MovementSpeed)
+            if (Entity.ModelRigidbody.isKinematic)
             {
-                Entity.Velocity = Entity.Velocity.normalized * Entity.MovementSpeed;
-            }
+                Vector3 moved = toMove.normalized * Entity.MovementSpeed * DeltaTime;
 
-            Vector3 moved = Entity.Velocity * DeltaTime;
-
-            if (Util.Approximately(toMove.sqrMagnitude, moved.sqrMagnitude) || toMove.sqrMagnitude <= moved.sqrMagnitude)
-            {
-				Entity.Position = m_vec3Destination;
-
-                return false;
+                if (Util.Approximately(toMove.sqrMagnitude, moved.sqrMagnitude) || toMove.sqrMagnitude <= moved.sqrMagnitude)
+                {
+                    Entity.Position = m_vec3Destination;
+                    return false;
+                }
+                else
+                {
+                    Entity.Position += moved;
+                    return true;
+                }
             }
             else
             {
-				Entity.Position += moved;
+                var force = toMove.XZ().normalized * Entity.MovementSpeed / 0.2f * DeltaTime;
 
-                return true;
+                Entity.ModelRigidbody.velocity += force;
+
+                if (Entity.ModelRigidbody.velocity.XZ().magnitude >= Entity.MovementSpeed)
+                {
+                    var xz = Entity.ModelRigidbody.velocity.XZ().normalized * Entity.MovementSpeed;
+
+                    Entity.ModelRigidbody.velocity = new Vector3(xz.x, Entity.ModelRigidbody.velocity.y, xz.z);
+                }
+
+                return --remainCount > 0;
             }
         }
 
         protected override void OnBehaviorEnd()
         {
             base.OnBehaviorEnd();
-
-			Entity.Velocity = Vector3.zero;
 
             Entity.SendCommandToViews(new AnimatorSetBool("Move", false));
         }
@@ -70,12 +73,14 @@ namespace Behavior
             base.SetData(nBehaviorMasterID);
 
             m_vec3Destination = (Vector3)param[0];
+            remainCount = 3;
         }
         #endregion
 
         public void SetDestination(Vector3 vec3Destination)
         {
             m_vec3Destination = vec3Destination;
+            remainCount = 3;
         }
 
         public Vector3 GetDestination()
