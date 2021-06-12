d046f60aa85d4f1cec6b85138a0cfbb63d4b3d06
re5nardo <re5nardo@naver.com>
2021-06-12 10:13:56 +0900

    [배인수] 1. PlayerMoveInput 다듬기

--- Assets/Scripts/Photon/PhotonData.cs
+++ Assets/Scripts/Photon/PhotonData.cs
@@ -267,13 +267,11 @@ public class PlayerMoveInput
     {
     }
 
-    public PlayerMoveInput(int tick = -1, long sequence = -1, int entityID = -1, SerializableVector3 position = default, SerializableVector3 rotation = default, SerializableVector3 inputData = default, InputType inputType = InputType.None)
+    public PlayerMoveInput(int tick = -1, long sequence = -1, int entityID = -1, SerializableVector3 inputData = default, InputType inputType = InputType.None)
     {
         this.tick = tick;
         this.sequence = sequence;
         this.entityID = entityID;
-        this.position = position;
-        this.rotation = rotation;
         this.inputData = inputData;
         this.inputType = inputType;
     }
@@ -281,8 +279,6 @@ public class PlayerMoveInput
     public int tick = -1;
     public long sequence = -1;
     public int entityID = -1;
-    public SerializableVector3 position = default;
-    public SerializableVector3 rotation = default;
     public SerializableVector3 inputData = default;
     public InputType inputType = InputType.None;
 }
