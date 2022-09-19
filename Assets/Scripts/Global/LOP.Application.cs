84cd69bc60c7cf9c2de5f28375e93dc7d98c86d4
re5nardo <re5nardo@naver.com>
2022-09-20 01:50:14 +0900

    Error / Exception 상황 시 로그 파일 남기도록 수정

--- Assets/Scripts/Global/LOP.Application.cs
+++ Assets/Scripts/Global/LOP.Application.cs
@@ -5,6 +5,7 @@ using UnityEngine.Networking;
 using System.Text.RegularExpressions;
 using GameFramework;
 using System.Threading.Tasks;
+using System;
 
 namespace LOP
 {
@@ -24,6 +25,18 @@ namespace LOP
 
         private static async void Initialize()
         {
+            UnityEngine.Application.logMessageReceived += (condition, stackTrace, type) =>
+            {
+                switch (type)
+                {
+                    case LogType.Error:
+                    case LogType.Exception:
+                        var path = System.IO.Path.Combine(UnityEngine.Application.persistentDataPath, $"{DateTime.Now.ToLongTimeString()}.txt");
+                        System.IO.File.WriteAllText(path, stackTrace);
+                        break;
+                }
+            };
+
             //  Target FrameRate
             UnityEngine.Application.targetFrameRate = 60;
 
