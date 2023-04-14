c9f2fcad350d0a2942937da911a2abe761dcd91e
re5nardo <re5nardo@naver.com>
2023-04-14 22:10:56 +0900

    WebRequest 사용하는 구조로 수정

--- Assets/Scripts/LOPWeb/LOPWebAPI.cs
+++ Assets/Scripts/LOPWeb/LOPWebAPI.cs
@@ -2,44 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 using GameFramework;
-using System;
 
 public class LOPWebAPI
 {
-    public static HttpRequestContainer<string> Heartbeat(string roomId, Action<string> onResult = null, Action<string> onError = null)
+    public static WebRequest<string> Heartbeat(string roomId)
     {
-        return Http.Put($"room/heartbeat/{roomId}", onResult, onError, apiSettings: GameFramework.ServerSettings.Get("ServerSettings_Room"));
+        return new WebRequestBuilder<string>()
+            .SetUri(GameFramework.ServerSettings.Get("ServerSettings_Room").GetFullUri($"room/heartbeat/{roomId}"))
+            .SetMethod(HttpMethod.PUT)
+            .Build();
     }
 
-    public static HttpRequestContainer<string> NotifyStartServer(NotifyStartServerRequest request, Action<string> onResult = null, Action<string> onError = null)
+    public static WebRequest<string> NotifyStartServer(NotifyStartServerRequest request)
     {
-        return Http.Put($"room", JsonUtility.ToJson(request), onResult, onError, apiSettings: GameFramework.ServerSettings.Get("ServerSettings_Room"));
+        return new WebRequestBuilder<string>()
+            .SetUri(GameFramework.ServerSettings.Get("ServerSettings_Room").GetFullUri($"room"))
+            .SetMethod(HttpMethod.PUT)
+            .SetRequestBody(request)
+            .Build();
     }
 
-    public static HttpRequestContainer<string> NotifyStopServer(string roomId, Action<string> onResult = null, Action<string> onError = null)
+    public static WebRequest<string> NotifyStopServer(string roomId)
     {
-        return Http.Delete($"room/{roomId}", onResult, onError, apiSettings: GameFramework.ServerSettings.Get("ServerSettings_Room"));
+        return new WebRequestBuilder<string>()
+            .SetUri(GameFramework.ServerSettings.Get("ServerSettings_Room").GetFullUri($"room/{roomId}"))
+            .SetMethod(HttpMethod.DELETE)
+            .Build();
     }
 
-    public static HttpRequestContainer<UpdateRoomStatusResult> UpdateRoomStatus(UpdateRoomStatusRequest request, Action<UpdateRoomStatusResult> onResult = null, Action<string> onError = null)
+    public static WebRequest<UpdateRoomStatusResult> UpdateRoomStatus(UpdateRoomStatusRequest request)
     {
-        return Http.Put($"room/status", JsonUtility.ToJson(request), onResult, onError, apiSettings: GameFramework.ServerSettings.Get("ServerSettings_Room"));
+        return new WebRequestBuilder<UpdateRoomStatusResult>()
+            .SetUri(GameFramework.ServerSettings.Get("ServerSettings_Room").GetFullUri($"room/status"))
+            .SetMethod(HttpMethod.PUT)
+            .SetRequestBody(request)
+            .Build();
     }
 
     #region Match
-    public static HttpRequestContainer<GetMatchResult> GetMatch(string matchId, Action<GetMatchResult> onResult = null, Action<string> onError = null)
+    public static WebRequest<GetMatchResult> GetMatch(string matchId)
     {
-        return Http.Get($"match/{matchId}", onResult, onError, apiSettings: GameFramework.ServerSettings.Get("ServerSettings_Room"));
+        return new WebRequestBuilder<GetMatchResult>()
+            .SetUri(GameFramework.ServerSettings.Get("ServerSettings_Room").GetFullUri($"match/{matchId}"))
+            .SetMethod(HttpMethod.GET)
+            .Build();
     }
 
-    public static HttpRequestContainer<MatchStartResult> MatchStart(MatchStartRequest request, Action<MatchStartResult> onResult = null, Action<string> onError = null)
+    public static WebRequest<MatchStartResult> MatchStart(MatchStartRequest request)
     {
-        return Http.Put($"match/match-start", JsonUtility.ToJson(request), onResult, onError, apiSettings: GameFramework.ServerSettings.Get("ServerSettings_Room"));
+        return new WebRequestBuilder<MatchStartResult>()
+            .SetUri(GameFramework.ServerSettings.Get("ServerSettings_Room").GetFullUri($"match/match-start"))
+            .SetMethod(HttpMethod.PUT)
+            .SetRequestBody(request)
+            .Build();
     }
 
-    public static HttpRequestContainer<MatchEndResult> MatchEnd(MatchEndRequest request, Action<MatchEndResult> onResult = null, Action<string> onError = null)
+    public static WebRequest<MatchEndResult> MatchEnd(MatchEndRequest request)
     {
-        return Http.Put($"match/match-end", JsonUtility.ToJson(request), onResult, onError, apiSettings: GameFramework.ServerSettings.Get("ServerSettings_Room"));
+        return new WebRequestBuilder<MatchEndResult>()
+            .SetUri(GameFramework.ServerSettings.Get("ServerSettings_Room").GetFullUri($"match/match-end"))
+            .SetMethod(HttpMethod.PUT)
+            .SetRequestBody(request)
+            .Build();
     }
     #endregion
 }
